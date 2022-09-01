8e40fc07d65bc0803f40bb5d00352f9b630dffb8
Linkgza <109047450+Linkgza@users.noreply.github.com>
2022-08-31 22:40:23 -0500

    Movie Information

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, UseMediaQuery, Rating } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
-const MovieInformation = () => {
-    console.log('Movie Information');
-    
+import { useStyles } from './styles';
+import { useGetMoviesQuery } from '../../services/TMDB';
+
+function MovieInformation() {
+  const { data, isFetching, error } = useGetMoviesQuery(id);
+  const { id } = useParams();
+  const classes = useStyles();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+
   return (
     <div>
-     MovieInformation
+      <Grid container className={classes.cointainerSpaceAround}>
+        <Grid item sm={12} lg={4}>
+          <img
+            className={classes.poster}
+            src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+            alt={data?.title}
+          />
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default MovieInformation
+export default MovieInformation;
