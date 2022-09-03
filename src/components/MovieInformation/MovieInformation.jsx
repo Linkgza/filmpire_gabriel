c4a17927c9f62dce695f1cfafbb70062eea3e93b
Linkgza <109047450+Linkgza@users.noreply.github.com>
2022-09-03 14:12:29 -0500

    Update MovieInformation.jsx

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -29,6 +29,8 @@ function MovieInformation() {
     );
   }
 
+  console.log(data);
+
   return (
     <Grid container className={classes.containerSpaceAround}>
       <Grid item sm={12} lg={4}>
@@ -37,6 +39,25 @@ function MovieInformation() {
           src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
           alt={data?.title}
         />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.cointainerSpaceAround}>
+          <Box display="flex" justifyContent="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography variant="subtitle1" gutterBottom style={{ marginLeft: '10px' }}>
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
