7545a68194383c32a1116849ff316113052b6d6e
Linkgza <109047450+Linkgza@users.noreply.github.com>
2022-09-07 12:16:36 -0500

    Movie Information pt2

--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -41,6 +41,7 @@ export default makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    textDecoration: 'none',
     [theme.breakpoints.down('sm')]: {
       padding: '0.5rem 1rem',
     },
@@ -51,5 +52,13 @@ export default makeStyles((theme) => ({
     height: '8rem',
     objectFit: 'cover',
     borderRadius: '10px',
+  },
+  buttonsContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
   },
 }));
